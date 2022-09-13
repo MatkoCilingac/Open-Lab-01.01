96764fee5bf6e76638e966926819f5cbe7431dcc
Martin <113422018+MatkoCilingac@users.noreply.github.com>
2022-09-13 20:18:17 +0200

    Update Exercise.cs

--- Open-Lab-01.01/Exercise.cs
+++ Open-Lab-01.01/Exercise.cs
@@ -4,7 +4,7 @@ namespace Open_Lab_01._01
 {
     public class Exercise
     {
-        public bool IsLessThan10(string input)
+        public bool IsLessThan10(stringinput)
         {
             throw new NotImplementedException();
         }
